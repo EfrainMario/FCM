c2a46a34d4a9f3e3ef6de17263714ba96096cabc
EfrainMario <efrainmario345@gmail.com>
2019-01-17 12:54:18 +0100

    Teste 2

---
+++ app/src/main/java/exercicios/example/com/exericio1/service/MyFirebaseMessagingService.java
@@ -0,0 +1,19 @@
+package exercicios.example.com.exericio1.service;
+
+import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        Log.i("Log", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(token);
+    }
+
+
+}
